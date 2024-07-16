b5a8711627cc4816957fbed5c10f7d09142beb1f
Yunfan Li <lyf_liyunfan@sina.com>
2024-07-16 22:39:28 +0800

    Fix P1

--- src/observer/storage/record/record_manager.cpp
+++ src/observer/storage/record/record_manager.cpp
@@ -496,7 +496,7 @@ RC PaxRecordPageHandler::get_record(const RID &rid, Record &record)
 
   record.set_rid(rid);
 
-  char data[page_header_->record_size + 5];
+  char *data = (char *)malloc(page_header_->record_size + 10);
   int offset = 0;
   for (int col_num = 0; col_num < page_header_->column_num; col_num++) {
     char *colToPos = get_field_data(rid.slot_num, col_num);
